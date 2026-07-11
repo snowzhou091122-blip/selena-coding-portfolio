c10cab5f8b1d85d54de433a42e4d6044cfc24cc3
snowzhou091122-blip <snowzhou091122@gmail.com>
2026-07-11 16:37:13 +0800

    Create 49_group_anagrams.py
    Group anagrams by using each sorted word as a hashmap key.

---
+++ leetcode/hashmap/49_group_anagrams.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = {}
+
+        for word in strs:
+            key = "".join(sorted(word))
+            if key in hashmap:
+                hashmap[key].append(word)
+              else:
+                hashmap[key]=word
+              return list(hashmap.values())
+              
